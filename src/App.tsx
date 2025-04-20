55628dc00bb27f126a88df94055734e225475ad3
Shreyash Dhapke <shreyash@utilizecore.com>
2025-04-20 19:44:57 +0530

    Fixed issue of navigation from headers

--- src/App.tsx
+++ src/App.tsx
@@ -1,73 +1,48 @@
-import { Component } from "react";
-import "./App.css";
-import { BrowserRouter, Route, Routes, useLocation } from "react-router-dom";
-import { CSSTransition, TransitionGroup } from "react-transition-group"; // Import transition components
-import { Box } from "@mui/material";
-import ScrollToTop from "./components/common/scroll-to-top";
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { Box, CssBaseline } from "@mui/material";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
-import LoginPage from "./views/login";
+
+import "./App.css";
+import ScrollToTop from "./components/common/scroll-to-top";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./views/home";
+import LoginPage from "./views/login";
 import SignupPage from "./views/signup";
 import ForgotPasswordPage from "./views/forgot-password";
 
+// Theme config
 const theme = createTheme({
   typography: {
     fontFamily: "Roboto, sans-serif",
   },
 });
 
-function AppRoutes() {
-  const location = useLocation();
-
+function App() {
   return (
-    <TransitionGroup component={null}>
-      <CSSTransition
-        key={location.pathname} // Ensure animation triggers on location change
-        timeout={500} // Set timeout for the animation
-        classNames="page" // Use this class for animations
-      >
-        <Box>
-          <Routes location={location}>
-            <Route path="/">
-              <Route index element={<Home />} />
-            </Route>
-            <Route path="/login">
-              <Route index element={<LoginPage />} />
-            </Route>
-            <Route path="/signup">
-              <Route index element={<SignupPage />} />
-            </Route>
-            <Route path="/forgot-password">
-              <Route index element={<ForgotPasswordPage />} />
-            </Route>
-          </Routes>
-        </Box>
-      </CSSTransition>
-    </TransitionGroup>
-  );
-}
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <BrowserRouter>
+        <ScrollToTop />
+        <Box className="App">
+          <Box className="fixed-header">
+            <Header />
+          </Box>
 
-class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        <ThemeProvider theme={theme}>
-          <BrowserRouter>
-            <ScrollToTop />
-            <div className="fixed-header">
-              <Header />
-            </div>
-            <div className="main-content">
-              <AppRoutes />
-              <Footer />
-            </div>
-          </BrowserRouter>
-        </ThemeProvider>
-      </div>
-    );
-  }
+          <Box className="main-content">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<LoginPage />} />
+              <Route path="/signup" element={<SignupPage />} />
+              <Route path="/forgot-password" element={<ForgotPasswordPage />} />
+            </Routes>
+            <Footer />
+          </Box>
+        </Box>
+      </BrowserRouter>
+    </ThemeProvider>
+  );
 }
 
 export default App;
