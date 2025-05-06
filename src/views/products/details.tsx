5aaa24132e7bee437aac3e1d2a604421446edc43
Pravin Salame <pravin.salame@utilizecore.com>
2025-05-06 13:47:52 +0530

    updated code for products details

--- src/views/products/details.tsx
+++ src/views/products/details.tsx
@@ -60,9 +60,8 @@ export default function ProductDetails() {
 
   const [product, setProduct] = useState<Product>(initialProductData);
 
-  const handleTabChange = (event: any, newValue: any) => {
-    setTabValue(newValue);
-  };
+  const handleTabChange = (event: any, newValue: any) => setTabValue(newValue);
+
 
   const handleInputChange = (e: any) => {
     const { name, value } = e.target;
@@ -71,10 +70,8 @@ export default function ProductDetails() {
 
   const handleEdit = () => setIsEditing(true);
   const handleCancel = () => setIsEditing(false);
-  const handleSave = () => {
-    console.log("Saved data:", product);
-    setIsEditing(false);
-  };
+  const handleSave = () => setIsEditing(false)
+
   
   const responsiveBox = useMemo(() => ({ width: { xs: "100%", sm: "48%" } }), []);
   const textFieldStyles = useMemo(
@@ -88,275 +85,257 @@ export default function ProductDetails() {
     },
   }), []);
 
+  const CustomButton = ({ label, onClick, variant = "outlined", startIcon, color = "white" }: any) => (
+    <Button
+      onClick={onClick}
+      variant={variant}
+      startIcon={startIcon}
+      sx={{
+        color,
+        borderColor: "grey.600",
+        textTransform: "none",
+        "&:hover": {
+          backgroundColor: "grey.800",
+          borderColor: "white",
+        },
+      }}
+    >
+      {label}
+    </Button>
+  );
+  const ProductTabs = ({ value, onChange }: any) => (
+    <Tabs
+      value={value}
+      onChange={onChange}
+      textColor="inherit"
+      indicatorColor="primary"
+      sx={{ mb: 2 }}
+    >
+      <Tab label="Product Info" />
+      <Tab label="History" />
+    </Tabs>
+  );
+  
+  const editingFields = (
+    <>
+      {/* Product Info Card */}
+      <Card sx={{ bgcolor: "#2e2e2e", color: WHITE, mb: 4 }}>
+        <CardContent>
+        <CmxText label={product.name} align='left' isBold variant = "h5" sx={{ mb: 1 }}/>
+          <Box
+            display="flex"
+            flexDirection={{ xs: "column", sm: "row" }}
+            justifyContent="space-between"
+            alignItems="flex-start"
+            gap={2}
+          >
+            {/* Left side: Labels and Values */}
+            <Box display="flex">
+              <Box display="block">
+                <CmxText noWrap label='Product number :' align='left' variant = "subtitle1"/>
+                <CmxText noWrap label='Price excl. GST :' align='left' variant = "subtitle1"/>
+                <CmxText noWrap label='Tax code :' align='left' variant = "subtitle1"/>
+              </Box>
+              <Box sx={{ ml: 3 }}>
+                <CmxText noWrap label={product.productNo} align='left' variant = "subtitle1"/>
+                <CmxText noWrap label={product.price.toLocaleString()} align='left' variant = "subtitle1"/>
+                <CmxText noWrap label={product.taxCode} align='left' variant = "subtitle1"/>
+               
+              </Box>
+            </Box>
+
+            {/* Right side: Action Buttons */}
+            <Box
+              display="flex"
+              gap={2}
+              mt={{ xs: 2, sm: 0 }} // add margin top only in mobile
+            >
+              <CustomButton label="Edit" onClick={handleEdit} startIcon={<EditIcon />} />
+              <CustomButton label="Archive" startIcon={<ArchiveIcon />} />
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+
+      {/* Invoice Summary */}
+      <Card sx={{ bgcolor: "#3e3e3e", color: WHITE }}>
+        <CardContent>
+          <Box
+            display="flex"
+            justifyContent="space-around"
+            textAlign="center"
+            sx={{
+              flexDirection: {
+                xs: "column",
+                sm: "row",
+                lg: "row",
+                xl: "row",
+              },
+            }}
+          >
+            <Box>
+              <CmxText label='Last changed' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
+              <CmxText label={product?.updatedAt} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
+            </Box>
+            <Box>
+              <CmxText label='Times invoiced' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
+              <CmxText label={product?.timesInvoiced} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
+            </Box>
+            <Box>
+              <CmxText label='Total invoiced' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
+              <CmxText label={product?.totalInvoiced} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
+            </Box>
+          </Box>
+        </CardContent>
+      </Card>
+    </>
+  )
+
+  const notEditingFields = (
+    <>
+      {/* Edit Form */}
+      <Card sx={{ bgcolor: "#2e2e2e", p: 2 }}>
+        <CardContent>
+          <Box
+            component="form"
+            display="flex"
+            flexWrap="wrap"
+            gap={2}
+            justifyContent="space-between"
+          >
+            {/* Product No. */}
+            <Box sx={responsiveBox}>
+              <TextField
+                fullWidth
+                label="Product no."
+                name="productNo"
+                value={product.productNo}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputProps={{ style: { color: "white" } }}
+                sx={textFieldStyles}
+              />
+            </Box>
+
+            {/* Product Name */}
+            <Box sx={responsiveBox}>
+              <TextField
+                fullWidth
+                label="Product name (this will appear on the invoice) *"
+                name="productName"
+                value={product.name}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputProps={{ style: { color: "white" } }}
+                sx={textFieldStyles}
+              />
+            </Box>
+
+            {/* Tax Code */}
+            <Box sx={responsiveBox}>
+              <TextField
+                select
+                fullWidth
+                label="Tax code *"
+                name="taxCode"
+                value={product.taxCode}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputProps={{ style: { color: "white" } }}
+                sx={textFieldStyles}
+              >
+                {/* Example tax codes */}
+                <MenuItem value="0%">0% - Exports, Food, etc.</MenuItem>
+                <MenuItem value="5%">5% - Reduced rate</MenuItem>
+              </TextField>
+            </Box>
+
+            {/* GST */}
+            <Box sx={responsiveBox}>
+              <TextField
+                fullWidth
+                label="GST *"
+                name="gst"
+                value={product.taxCode}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      <PercentIcon sx={{ color: "white" }} />
+                    </InputAdornment>
+                  ),
+                  style: { color: "white" },
+                }}
+                sx={textFieldStyles}
+              />
+            </Box>
+
+            {/* Unit Price */}
+            <Box sx={responsiveBox}>
+              <TextField
+                fullWidth
+                label="Unit price *"
+                name="unitPrice"
+                value={product.price}
+                onChange={handleInputChange}
+                variant="outlined"
+                InputProps={{ style: { color: "white" } }}
+                sx={textFieldStyles}
+              />
+            </Box>
+
+            {/* GST Include/Exclude radio */}
+            <Box sx={responsiveBox}>
+              <FormControl component="fieldset">
+                <RadioGroup
+                  row
+                  name="gstIncluded"
+                  value={product.totalPrice}
+                  onChange={handleInputChange}
+                >
+                  <FormControlLabel
+                    value="included"
+                    control={<Radio sx={{ color: "white" }} />}
+                    label="Including GST"
+                    sx={{ color: "white" }}
+                  />
+                  <FormControlLabel
+                    value="excluded"
+                    control={<Radio sx={{ color: "white" }} />}
+                    label="Excluding GST"
+                    sx={{ color: "white" }}
+                  />
+                </RadioGroup>
+              </FormControl>
+            </Box>
+
+            {/* Total price */}
+            <Box sx={{ width: "100%", textAlign: "right", mt: 2 }}>
+              <CmxText label={`Total price (incl. GST): <strong>{product.totalPrice}</strong>`} variant="h6" sx={{ color: "white" }}/>
+            </Box>
+          </Box>
+
+          {/* Buttons */}
+          <Box display="flex" justifyContent="flex-end" gap={2} mt={4}>
+            <CustomButton label="Cancel" onClick={handleCancel} />
+            <CustomButton label="Save" onClick={handleSave} variant="contained" color="success" />
+          </Box>
+        </CardContent>
+      </Card>
+    </>
+  )
+
   return (
     <Box sx={{ p: 4, bgcolor: DARK_THEME_BG, minHeight: "100vh", color: "white" }}>
-      {/* Header */}
       <CmxText label={product.name} isBold variant = "h4" sx={{ mb: 2 }} />
-
-      {/* Tabs */}
-      <Tabs
-        value={tabValue}
-        onChange={handleTabChange}
-        textColor="inherit"
-        indicatorColor="primary"
-        sx={{ mb: 2 }}
-      >
-        <Tab label="Product Info" />
-        <Tab label="History" />
-      </Tabs>
+  
+      <ProductTabs value={tabValue} onChange={handleTabChange} />
 
       <Divider sx={{ borderColor: "grey.800", mb: 4 }} />
 
       {/* Content */}
-      {!isEditing ? (
-        <>
-          {/* Product Info Card */}
-          <Card sx={{ bgcolor: "#2e2e2e", color: WHITE, mb: 4 }}>
-            <CardContent>
-            <CmxText label={product.name} align='left' isBold variant = "h5" sx={{ mb: 1 }}/>
-              <Box
-                display="flex"
-                flexDirection={{ xs: "column", sm: "row" }}
-                justifyContent="space-between"
-                alignItems="flex-start"
-                gap={2}
-              >
-                {/* Left side: Labels and Values */}
-                <Box display="flex">
-                  <Box display="block">
-                    <CmxText noWrap label='Product number :' align='left' variant = "subtitle1"/>
-                    <CmxText noWrap label='Price excl. GST :' align='left' variant = "subtitle1"/>
-                    <CmxText noWrap label='Tax code :' align='left' variant = "subtitle1"/>
-                  </Box>
-                  <Box sx={{ ml: 3 }}>
-                    <CmxText noWrap label={product.productNo} align='left' variant = "subtitle1"/>
-                    <CmxText noWrap label={product.price.toLocaleString()} align='left' variant = "subtitle1"/>
-                    <CmxText noWrap label={product.taxCode} align='left' variant = "subtitle1"/>
-                   
-                  </Box>
-                </Box>
-
-                {/* Right side: Action Buttons */}
-                <Box
-                  display="flex"
-                  gap={2}
-                  mt={{ xs: 2, sm: 0 }} // add margin top only in mobile
-                >
-                  <Button
-                    onClick={handleEdit}
-                    variant="outlined"
-                    startIcon={<EditIcon />}
-                    sx={{
-                      color: "white",
-                      borderColor: "grey.600",
-                      textTransform: "none",
-                      "&:hover": {
-                        backgroundColor: "grey.800",
-                        borderColor: "white",
-                      },
-                    }}
-                  >
-                    Edit
-                  </Button>
-
-                  <Button
-                    variant="outlined"
-                    startIcon={<ArchiveIcon />}
-                    sx={{
-                      color: "white",
-                      borderColor: "grey.600",
-                      textTransform: "none",
-                      "&:hover": {
-                        backgroundColor: "grey.800",
-                        borderColor: "white",
-                      },
-                    }}
-                  >
-                    Archive
-                  </Button>
-                </Box>
-              </Box>
-            </CardContent>
-          </Card>
-
-          {/* Invoice Summary */}
-          <Card sx={{ bgcolor: "#3e3e3e", color: WHITE }}>
-            <CardContent>
-              <Box
-                display="flex"
-                justifyContent="space-around"
-                textAlign="center"
-                sx={{
-                  flexDirection: {
-                    xs: "column",
-                    sm: "row",
-                    lg: "row",
-                    xl: "row",
-                  },
-                }}
-              >
-                <Box>
-                  <CmxText label='Last changed' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
-                  <CmxText label={product?.updatedAt} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
-                </Box>
-                <Box>
-                  <CmxText label='Times invoiced' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
-                  <CmxText label={product?.timesInvoiced} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
-                </Box>
-                <Box>
-                  <CmxText label='Total invoiced' variant = "body2" sx={{ mt: { xs: 1, sm: 0, lg: 0, xl: 0 }}}/>
-                  <CmxText label={product?.totalInvoiced} variant = "h6" sx={{ mt: { xs: 0, sm: 1, lg: 1, xl: 1 }}}/>
-                </Box>
-              </Box>
-            </CardContent>
-          </Card>
-        </>
-      ) : (
-        <>
-          {/* Edit Form */}
-          <Card sx={{ bgcolor: "#2e2e2e", p: 2 }}>
-            <CardContent>
-              <Box
-                component="form"
-                display="flex"
-                flexWrap="wrap"
-                gap={2}
-                justifyContent="space-between"
-              >
-                {/* Product No. */}
-                <Box sx={responsiveBox}>
-                  <TextField
-                    fullWidth
-                    label="Product no."
-                    name="productNo"
-                    value={product.productNo}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    InputProps={{ style: { color: "white" } }}
-                    sx={textFieldStyles}
-                  />
-                </Box>
-
-                {/* Product Name */}
-                <Box sx={responsiveBox}>
-                  <TextField
-                    fullWidth
-                    label="Product name (this will appear on the invoice) *"
-                    name="productName"
-                    value={product.name}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    InputProps={{ style: { color: "white" } }}
-                    sx={textFieldStyles}
-                  />
-                </Box>
-
-                {/* Tax Code */}
-                <Box sx={responsiveBox}>
-                  <TextField
-                    select
-                    fullWidth
-                    label="Tax code *"
-                    name="taxCode"
-                    value={product.taxCode}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    InputProps={{ style: { color: "white" } }}
-                    sx={textFieldStyles}
-                  >
-                    {/* Example tax codes */}
-                    <MenuItem value="0%">0% - Exports, Food, etc.</MenuItem>
-                    <MenuItem value="5%">5% - Reduced rate</MenuItem>
-                  </TextField>
-                </Box>
-
-                {/* GST */}
-                <Box sx={responsiveBox}>
-                  <TextField
-                    fullWidth
-                    label="GST *"
-                    name="gst"
-                    value={product.taxCode}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    InputProps={{
-                      endAdornment: (
-                        <InputAdornment position="end">
-                          <PercentIcon sx={{ color: "white" }} />
-                        </InputAdornment>
-                      ),
-                      style: { color: "white" },
-                    }}
-                    sx={textFieldStyles}
-                  />
-                </Box>
-
-                {/* Unit Price */}
-                <Box sx={responsiveBox}>
-                  <TextField
-                    fullWidth
-                    label="Unit price *"
-                    name="unitPrice"
-                    value={product.price}
-                    onChange={handleInputChange}
-                    variant="outlined"
-                    InputProps={{ style: { color: "white" } }}
-                    sx={textFieldStyles}
-                  />
-                </Box>
-
-                {/* GST Include/Exclude radio */}
-                <Box sx={responsiveBox}>
-                  <FormControl component="fieldset">
-                    <RadioGroup
-                      row
-                      name="gstIncluded"
-                      value={product.totalPrice}
-                      onChange={handleInputChange}
-                    >
-                      <FormControlLabel
-                        value="included"
-                        control={<Radio sx={{ color: "white" }} />}
-                        label="Including GST"
-                        sx={{ color: "white" }}
-                      />
-                      <FormControlLabel
-                        value="excluded"
-                        control={<Radio sx={{ color: "white" }} />}
-                        label="Excluding GST"
-                        sx={{ color: "white" }}
-                      />
-                    </RadioGroup>
-                  </FormControl>
-                </Box>
-
-                {/* Total price */}
-                <Box sx={{ width: "100%", textAlign: "right", mt: 2 }}>
-                  <CmxText label={`Total price (incl. GST): <strong>{product.totalPrice}</strong>`} variant="h6" sx={{ color: "white" }}/>
-                </Box>
-              </Box>
-
-              {/* Buttons */}
-              <Box display="flex" justifyContent="flex-end" gap={2} mt={4}>
-                <Button
-                  onClick={handleCancel}
-                  variant="outlined"
-                  sx={{ color: "white", borderColor: "grey.500" }}
-                >
-                  Cancel
-                </Button>
-                <Button
-                  onClick={handleSave}
-                  variant="contained"
-                  color="success"
-                >
-                  Save
-                </Button>
-              </Box>
-            </CardContent>
-          </Card>
-        </>
-      )}
+      {isEditing ? editingFields : notEditingFields}
     </Box>
   );
 }
